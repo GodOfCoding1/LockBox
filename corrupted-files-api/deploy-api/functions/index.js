df664c47f40b9d3edd1f63f87b052d605ac4e087
Madhav V Rajeev <75824625+GodOfCoding1@users.noreply.github.com>
2024-01-29 18:48:31 +0530

    added firebase functions

---
+++ corrupted-files-api/deploy-api/functions/index.js
@@ -0,0 +1,25 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.get("/test", (req, res) => {
+  res.send(" Hooray, it works!");
+});
+exports.app = functions.https.onRequest(app);
